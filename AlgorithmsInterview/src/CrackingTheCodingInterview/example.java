ce481e8ad86710859675da94323df7ba682c3f69
Karthik <Karthik@KARTHIK>
2017-01-26 21:30:28 -0500

    7th commit

--- AlgorithmsInterview/src/CrackingTheCodingInterview/example.java
+++ AlgorithmsInterview/src/CrackingTheCodingInterview/example.java
@@ -2,6 +2,6 @@ package CrackingTheCodingInterview;
 
 public class example {
 	public static void main(String[] aa) {
-		System.out.println("I changed it");
+		System.out.println("I changed it again");
 	}
 }
